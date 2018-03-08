ed6461df42ad3089094ab1f3594b5df800e67507
zhihao.zhang <zhihao.zhang@quaie.com>
2018-03-08 14:06:03 +0800

    添加mechanize依赖,简单实现话题用户评论的抓取及展示

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,23 @@
 class WelcomeController < ApplicationController
+
+  BASIC_TOPICS_URI = 'https://ruby-china.org/topics?page='
+  BASIC_COMMENTS_URI = 'https://ruby-china.org/topics/'
+
   def index
+  end
+
+  def show_topics
+    page = params[:page].to_i
+    uri = BASIC_TOPICS_URI + page.to_s
+    @topics = Mechanize.new.get(uri).search("div#main div.title a").to_a
+    @users = Mechanize.new.get(uri).search("div#main div.info a").to_a
+    @hrefs = Mechanize.new.get(uri).search("div#main div.title a").to_a
+  end
+
+  def show_comments
+    topic_id = params[:topic_id]
+    uri = BASIC_COMMENTS_URI + topic_id
+    @comment_authors = Mechanize.new.get(uri).search("div#replies span.name a").to_a
+    @comment_contents = Mechanize.new.get(uri).search("div#replies div.markdown p").to_a
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/show_topics'
+  get 'welcome/show_comments'
+  
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
